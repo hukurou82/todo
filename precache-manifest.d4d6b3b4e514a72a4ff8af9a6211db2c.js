b12201135107cc8d845670a19684b2e4672e35c7
hukurou82 <hukurou82@gmail.com>
2020-04-23 18:21:21 +0900

    Updates

---
+++ precache-manifest.d4d6b3b4e514a72a4ff8af9a6211db2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ecb24938025ce0933497d11fc4cbca",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "25670786509e24f3c8d8",
+    "url": "/todo/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0550ed85191fad434b92",
+    "url": "/todo/static/js/2.e00be7c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.e00be7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25670786509e24f3c8d8",
+    "url": "/todo/static/js/main.2bead2ed.chunk.js"
+  },
+  {
+    "revision": "cc01816309df281be5e9",
+    "url": "/todo/static/js/runtime-main.4b16cc92.js"
+  }
+]);
